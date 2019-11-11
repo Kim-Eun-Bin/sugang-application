bf8c272fa6446f82abc1c16e60040ad547ac2fc4
shinsob822 <shinsob822@gmail.com>
2019-11-11 10:28:56 +0900

    기존 코드 복구

--- app/src/main/java/com/tpj/teamproject/MypageActivity.java
+++ app/src/main/java/com/tpj/teamproject/MypageActivity.java
@@ -1,28 +1,29 @@
 package com.tpj.teamproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MypageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_mypage);
         Button btn1 = (Button) findViewById(R.id.btn1);
         Button btn2 = (Button) findViewById(R.id.btn2);
         Button btn3 = (Button) findViewById(R.id.btn3);
         Button btn4 = (Button) findViewById(R.id.btn4);
-
+        Button btn5 = (Button) findViewById(R.id.btn5);
     }
 
     public void onClick1(View view) {
-
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 
     public void onClick2(View view) {
@@ -37,6 +38,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClick4(View view) {
         Intent intent = new Intent(this,MypageActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClick5(View view) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:01063032666"));
         startActivity(intent);
     }
 }
