3a25cfe93bdcbc3e61e1e96425569b509dfc7d12
shinsob822 <shinsob822@gmail.com>
2019-11-17 17:59:13 +0900

    달력추가

--- app/src/main/java/com/tpj/teamproject/AlarmActivity.java
+++ app/src/main/java/com/tpj/teamproject/AlarmActivity.java
@@ -2,17 +2,29 @@ package com.tpj.teamproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AlarmActivity extends AppCompatActivity {
-
+    private  static  final String TAG = "AlarmActivity";
+    private CalendarView mCalenderView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
+        mCalenderView = (CalendarView)findViewById(R.id.calendarView);
+        mCalenderView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String data = year + "/" + month + "/" + dayOfMonth;
+                Log.d(TAG, "onSelectedDayChange: date: "+data);
+            }
+        });
         Button btn1 = (Button) findViewById(R.id.btn1);
         Button btn2 = (Button) findViewById(R.id.btn2);
         Button btn3 = (Button) findViewById(R.id.btn3);
